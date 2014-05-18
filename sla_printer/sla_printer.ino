1b7c73e2695baf6d3548496974660d5e69660776
JimYTC <solofat@gmail.com>
2014-05-18 21:20:30 +0800

    Use AccelStepper for Platform stepper

--- sla_printer/sla_printer.ino
+++ sla_printer/sla_printer.ino
@@ -1,6 +1,7 @@
 #include <Stepper.h>
 #include <Servo.h>
 #include "config.h"
+#include "AccelStepper.h"
 
 /*** Program variables ***/
 // For Serial
@@ -8,34 +9,48 @@ char buffer[MAX_BUF];
 int sofar;
 
 // For Stepper motor
-Stepper stepper(STEPS_PER_MOTOR_REVOLUTION, STEPS_MOTOR_PIN_1, STEPS_MOTOR_PIN_3, STEPS_MOTOR_PIN_2, STEPS_MOTOR_PIN_4);
-int Steps2Take;
+Stepper stepper(STEPS_PER_MOTOR_REVOLUTION, STEPS_MOTOR_PIN_1, STEPS_MOTOR_PIN_2);
 char command;
 
-// For Servo motor
-Servo servo;
-int angle = SERVO_ANGLE_MIN;
+// For Rotation
+AccelStepper stepperRotate(AccelStepper::DRIVER, STEPS_MOTOR_PIN_3, STEPS_MOTOR_PIN_4);
 
-void setup() {
+void setup()
+{
   Serial.begin(SERIAL_BAND);
   while (!Serial) {}
-  servo.attach(SERVO_MOTOR_PIN);
-  servo.write(0);
   help();
   stepper.setSpeed(STEPPER_SPEED);
+  stepperRotate.setMaxSpeed(MAX_SPEED);
+  stepperRotate.setAcceleration(ACC_SPEED);
+  stepperRotate.setSpeed(1);
   ready();
 }
 
-void loop() {
+void loop()
+{
+  if (stepperRotate.distanceToGo() != 0)
+  {
+    stepperRotate.run();
+    return;
+  }
+//  if (stepper.distnaceToGo() != 0)
+//  {
+//    stepper.runSpeedToPosition();
+//    return;
+//  }
+
   // listen for commands
-  while(Serial.available() > 0) {  // if something is available
+  while(Serial.available() > 0)
+  {  // if something is available
     char c = Serial.read();  // get it
     Serial.print(c);  // repeat it back so I know you got the message
     if(sofar < MAX_BUF) buffer[sofar++] = c;  // store it
     if(buffer[sofar-1] == ';') break;  // entire message received
   }
 
-  if(sofar > 0 && buffer[sofar-1] == ';') {
+  if(sofar > 0 && buffer[sofar-1] == ';')
+  {
     // we got a message and it ends with a semicolon
     buffer[sofar] = 0;  // end the buffer so string functions work right
     Serial.print(F("\r\n"));  // echo a return character for humans
@@ -45,14 +60,16 @@ void loop() {
 }
 
 // Print help messages
-void help() {
+void help()
+{
   Serial.print(F("SLA Printer ver: "));
   Serial.println(VERSION);
   Serial.println(F("Commands:"));
   Serial.println(F("G02 [Z(steps)]; - linear move up"));
   Serial.println(F("G03 [Z(steps)]; - linear move down"));
   Serial.println(F("G04 P[seconds]; - delay"));
-  Serial.println(F("M01 [A(angle)]; - rotate servo"));
+  Serial.println(F("M02 [Z(steps)]; - rotate servo"));
+  Serial.println(F("M03 [Z(steps)]; - rotate servo"));
   Serial.println(F("M100; - this help message"));
 }
 
@@ -60,7 +77,8 @@ void help() {
  * prepares the input buffer to receive a new message
  * and tells the serial connected device it is ready for more.
  */
-void ready() {
+void ready()
+{
   sofar=0;  // clear input buffer
   Serial.print(F(">"));  // signal ready to receive input
 }
@@ -68,7 +86,8 @@ void ready() {
 /**
  * Read the input buffer and find any recognized commands.  One G or M command per line.
  */
-void processCommand() {
+void processCommand()
+{
   // look for commands that start with 'G'
   int cmd = parsenumber('G',-1);
   int arg_value = 0;
@@ -84,12 +103,18 @@ void processCommand() {
   // look for commands that start with 'M'
   cmd=parsenumber('M', -1);
   switch(cmd) {
-  case  1:
-      arg_value = parsenumber('A', 0);
-      servo.write(arg_value);
-      delay(100);
-//      servo.write(0);
-      break;
+  case  2:
+    rotateCW(parsenumber('Z', 0));
+    break;
+  case  3:
+    rotateCCW(parsenumber('Z', 0));
+    break;
+  case 10:
+    Serial.print("pos: ");
+    Serial.println(stepperRotate.currentPosition());
+    Serial.print("target: ");
+    Serial.println(stepperRotate.targetPosition());
+    break;
   case 100:  help();  break;  // print help
   default:  break;
   }
@@ -103,10 +128,13 @@ void processCommand() {
  * @input code the character to look for.
  * @input val the return value if /code/ is not found.
  **/
-float parsenumber(char code, float val) {
+float parsenumber(char code, float val)
+{
   char *ptr=buffer;
-  while(ptr && *ptr && ptr<buffer+sofar) {
-    if(*ptr==code) {
+  while(ptr && *ptr && ptr<buffer+sofar)
+  {
+    if(*ptr==code)
+    {
       return atof(ptr+1);
     }
     ptr=strchr(ptr,' ')+1;
@@ -119,7 +147,8 @@ float parsenumber(char code, float val) {
  * @input code the string.
  * @input val the float.
  */
-void output(char *code, float val) {
+void output(char *code, float val)
+{
   Serial.print(code);
   Serial.println(val);
 }
@@ -128,6 +157,22 @@ void output(char *code, float val) {
  * Pause devices
  * @input wait_millis the milli-seconds to wait
  */
- void pause(int wait_millis) {
-   delay(wait_millis);
- }
+void pause(int wait_millis)
+{
+  delay(wait_millis);
+}
+
+void rotateCW(int steps) {
+    rotate(steps);
+}
+
+void rotateCCW(int steps) {
+  rotate(-1 * steps);
+}
+
+void rotate(int steps)
+{
+  stepperRotate.move(steps);
+//  stepperRotate.run();
+  stepperRotate.runSpeedToPosition();
+}
